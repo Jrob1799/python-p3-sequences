cb70548b4871d70aa5f67d56025a57d54d5c812a
Jacob <jacobwroberts33@gmail.com>
2023-07-26 13:14:54 -0400

    pass all tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [0,1]
+    while len(fibonacci_list) < length:
+        next_value = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(next_value)
+    print(fibonacci_list[0:length])
